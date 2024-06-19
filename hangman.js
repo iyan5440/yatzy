647bbfbafe1838d7159e405a3ac1476e590bb7c3
Iyanu Aketepe <55199789+iyan5440@users.noreply.github.com>
2024-06-19 15:28:40 -0400

    Updated keyboard: removed extraneous buttons, added button click events. Centered Keyboard

--- hangman.js
+++ hangman.js
@@ -2,7 +2,19 @@
 window.onload = function() {
     var wordList = document.getElementById("wordList");
 
+    const keyboardKeys = document.querySelectorAll('.key');
+
     for(let i = 0; i < 6; i++) { //wordlength
-		wordList.innerHTML += '<div class="word">' + 'A' + '</div>'; //'<div>_</div>' '<div>' + '_' + '</div>'
+		wordList.innerHTML += '<div class="word">' + '_' + '</div>'; //'<div>_</div>' '<div>' + '_' + '</div>'
 	}
+
+    keyboardKeys.forEach(keyboardKey => {
+        keyboardKey.addEventListener('click', () => {
+            console.log("Check: " + keyboardKey.innerHTML);
+        })
+    });
+}
+
+function check(character) {
+    
 }
