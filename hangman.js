7692b24bfbff13115808386c207b6fac00097cd9
NatNatNutmeg <81168884+NatNatNutmeg@users.noreply.github.com>
2024-06-19 18:41:51 -0400

    Added code for guessing wrong

--- hangman.js
+++ hangman.js
@@ -1,20 +1,20 @@
-
-window.onload = function() {
-    var wordList = document.getElementById("wordList");
-
-    const keyboardKeys = document.querySelectorAll('.key');
-
-    for(let i = 0; i < 6; i++) { //wordlength
-		wordList.innerHTML += '<div class="word">' + '_' + '</div>'; //'<div>_</div>' '<div>' + '_' + '</div>'
-	}
-
-    keyboardKeys.forEach(keyboardKey => {
-        keyboardKey.addEventListener('click', () => {
-            console.log("Check: " + keyboardKey.innerHTML);
-        })
-    });
-}
-
-function check(character) {
-    
+//Javascript
+const IDs = ["https://pbs.twimg.com/media/GQdun7FXwAABl16?format=png&name=900x900",
+    "https://pbs.twimg.com/media/GQdk8QrXwAADygT?format=png&name=900x900",
+    "https://pbs.twimg.com/media/GQdk6kfW0AEV_5U?format=png&name=900x900",
+    "https://pbs.twimg.com/media/GQdk6kmWgAA07ea?format=png&name=900x900",
+    "https://pbs.twimg.com/media/GQdk6kgXAAAJny8?format=png&name=900x900",
+    "https://pbs.twimg.com/media/GQdk6kgW4AAulJG?format=png&name=900x900",
+    "https://pbs.twimg.com/media/GQdk8Q5WYAAsB7f?format=png&name=900x900"];
+let fails = 0;
+const stickman = main.querySelector("stickman");
+function tryLetter(){
+    if(fail){
+        fails++;
+        stickman.removeElement("src");
+        stickman.src=IDs[fails]
+        if (fails==6){
+            lose();
+        }
+    }
 }
