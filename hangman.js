c5335f0be3ae88bdd0ae75d1496414b38f086289
Iyanu Aketepe <55199789+iyan5440@users.noreply.github.com>
2024-06-19 17:37:12 -0400

    Updated Keyboard to remove correct guess keys, removed space bar, added in random word functionality

--- hangman.js
+++ hangman.js
@@ -1,20 +1,44 @@
 
 window.onload = function() {
-    var wordList = document.getElementById("wordList");
+    var unknownString = document.getElementById("unknownString");
+
+    const wordList = ['AZURE','DIRNDI','LYMPH','BUFFOON','PLAIN','DUPLEX','ZILCH','EMBEZZLE','SPHINX','ESPIONAGE','EUOUAE'];
+
+    const chosen = wordList[Math.floor(Math.random() * wordList.length)];
+
+    console.log("" + chosen);
 
     const keyboardKeys = document.querySelectorAll('.key');
 
-    for(let i = 0; i < 6; i++) { //wordlength
-		wordList.innerHTML += '<div class="word">' + '_' + '</div>'; //'<div>_</div>' '<div>' + '_' + '</div>'
+    for(let i = 0; i < chosen.length; i++) {
+		unknownString.innerHTML += '<div class="word">' + '_' + '</div>';
 	}
+
+    const words = unknownString.children;
 
     keyboardKeys.forEach(keyboardKey => {
         keyboardKey.addEventListener('click', () => {
             console.log("Check: " + keyboardKey.innerHTML);
+            checkLetter(keyboardKey, chosen, words);
         })
     });
 }
 
-function check(character) {
-    
+function checkLetter(userKey, chosen, words) {
+    userChar = userKey.innerHTML
+    if(chosen.indexOf(userChar) > -1) {
+        findAndUpdate(userChar, chosen, words);
+        userKey.style.visibility = 'hidden';
+    }
+    else {
+        //damage hangman
+    }
 }
+        
+function findAndUpdate(userChar, chosen, words) {
+    for(let i = 0; i< chosen.length; i++) {
+        if(userChar === chosen.charAt(i)) {
+            words[i].innerHTML = userChar;
+        }
+    }
+}          
