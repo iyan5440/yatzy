a0a4a287b74687bd0f789d0252bce17668ccf2b3
Iyanu Aketepe <55199789+iyan5440@users.noreply.github.com>
2024-08-04 00:05:27 -0400

    Recovered from merge

--- public/index.js
+++ public/index.js
@@ -1,141 +1,132 @@
-//click event for start button
-    //post
-    //UserName from input
-
-    onload = () => {
-        getLeaderboard();
-    }
+onload = () => {
+    getLeaderboard();
+}
 
 
 
-    //const userName = document.getElementById("user-input");   
-    var userName, potUser, potPass;
-    
-    const start = document.getElementById("start-button");
-    const userInput = document.getElementById("user-input");
-    const potUserInput = document.getElementById("potentialAdminUser");
-    const potPassInput = document.getElementById("potentialAdminPass");
+//const userName = document.getElementById("user-input");   
+var userName, potUser, potPass;
 
-    var leaderboardHtml = document.getElementById("leaderboard");
-    var adminDiv = document.getElementById("admin-div");
-    var popout = document.getElementById("popout");
-    var msg = document.getElementById("msg");
-  
+const start = document.getElementById("start-button");
+const userInput = document.getElementById("user-input");
+const potUserInput = document.getElementById("potentialAdminUser");
+const potPassInput = document.getElementById("potentialAdminPass");
 
-    const keyboardKeys = document.querySelectorAll('.key');
-
-    //const unknownLetters = unknownString.children; //unknownLetters
+var leaderboardHtml = document.getElementById("leaderboard");
+var adminDiv = document.getElementById("admin-div");
+var msg = document.getElementById("msg");
 
 
-    keyboardKeys.forEach(keyboardKey => {
-        keyboardKey.addEventListener('click', () => {
-            //console.log("Check: " + keyboardKey.innerHTML);
-            checkLetter(keyboardKey.innerText).then( resu => {
-                update(resu, keyboardKey);
-            })
-            
-            //get win, lose or update
+const keyboardKeys = document.querySelectorAll('.key');
+
+//const unknownLetters = unknownString.children; //unknownLetters
+
+keyboardKeys.forEach(keyboardKey => {
+    keyboardKey.addEventListener('click', () => {
+        //console.log("Check: " + keyboardKey.innerHTML);
+        checkLetter(keyboardKey.innerText).then( resu => {
+            update(resu, keyboardKey);
         })
-    });
-    
+        
+        //get win, lose or update
+    })
+});
 
-    function togglePopout() {
-        adminDiv.hidden = !adminDiv.hidden;
-        popout.hidden = !popout.hidden;
+
+
+
+function verifyStartGameState() {
+    userName = userInput.value.trim();
+    console.log(userName);
+
+    //console.log(userName == "");
+    if (userName !== "") {
+        sendUserName(userName);
+        location.replace("main.php");
+
     }
+}
 
-    function verifyStartGameState() {
-        userName = userInput.value.trim();
-        console.log(userName);
+function sendUserName(userName) {
+    const xmlhttp = new XMLHttpRequest();
 
-        //console.log(userName == "");
-        if (userName !== "") {
-            sendUserName(userName);
-            location.replace("main.php");
-
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == XMLHttpRequest.DONE) {
+            if (xmlhttp.status == 200) {
+                const res = JSON.parse(xmlhttp.responseText);
+                console.log(res.messaged);
+            }
         }
-    }
+    };
 
-    function sendUserName(userName) {
-        const xmlhttp = new XMLHttpRequest();
+    const encodedUserName = encodeURIComponent(userName);
+    xmlhttp.open("POST", `./Hangman-api.php?action=sendUserName&userName=${encodedUserName}`, true);
+    xmlhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xmlhttp.send();
+}
 
-        xmlhttp.onreadystatechange = function() {
-            if (xmlhttp.readyState == XMLHttpRequest.DONE) {
-                if (xmlhttp.status == 200) {
-                    const res = JSON.parse(xmlhttp.responseText);
-                    console.log(res.messaged);
+function checkIfAdmin(potUser, potPass) {
+    potUser = potUserInput.value;
+    potPass = potPassInput.value;
+
+    const xmlhttp = new XMLHttpRequest();
+
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == XMLHttpRequest.DONE) {
+            if (xmlhttp.status == 200) {
+                const res = JSON.parse(xmlhttp.responseText);
+                if( res.adminStatus == false) {
+                    msg.innerText = "ERROR INCORRECT USER AND OR PASSWORD";
+                } else {
+                    msg.innerText = "";
+                    location.replace(res.adminRedirect);
                 }
+                
+                
             }
-        };
+        }
+    };
+    const encodedPotUser = encodeURIComponent(potUser);
+    const encodedPotPass = encodeURIComponent(potPass);
+    xmlhttp.open("GET", `./Hangman-api.php?action=checkIfAdmin&potUser=${encodedPotUser}&potPass=${encodedPotPass}`, true);
+    xmlhttp.send();
 
-        const encodedUserName = encodeURIComponent(userName);
-        xmlhttp.open("POST", `./Hangman-api.php?action=sendUserName&userName=${encodedUserName}`, true);
-        xmlhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-        xmlhttp.send();
-    }
+}
 
-    function checkIfAdmin(potUser, potPass) {
-        potUser = potUserInput.value;
-        potPass = potPassInput.value;
+//have a get request that adds information from server to leaderboard
 
-        const xmlhttp = new XMLHttpRequest();
+function getLeaderboard() {
+    const xmlhttp = new XMLHttpRequest();
 
-        xmlhttp.onreadystatechange = function() {
-            if (xmlhttp.readyState == XMLHttpRequest.DONE) {
-                if (xmlhttp.status == 200) {
-                    const res = JSON.parse(xmlhttp.responseText);
-                    if( res.adminStatus == false) {
-                        msg.innerText = "ERROR INCORRECT USER AND OR PASSWORD";
-                    } else {
-                        msg.innerText = "";
-                        location.replace(res.adminRedirect);
-                    }
-                    
-                    
-                }
-            }
-        };
-        const encodedPotUser = encodeURIComponent(potUser);
-        const encodedPotPass = encodeURIComponent(potPass);
-        xmlhttp.open("GET", `./Hangman-api.php?action=checkIfAdmin&potUser=${encodedPotUser}&potPass=${encodedPotPass}`, true);
-        xmlhttp.send();
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == XMLHttpRequest.DONE) {
+            if (xmlhttp.status == 200) {
+                const res = JSON.parse(xmlhttp.responseText);
 
-    }
+                leaderboardRes = res.leaderboard; 
+                // update leaderboard table with leaderboardRes content
+                for (let i = 0; i < leaderboardRes.length; i++) {
+                    const curRow = leaderboardRes[i];
+                    const newRow = document.createElement('tr');
 
-    //have a get request that adds information from server to leaderboard
+                    const nameCell = document.createElement('td');
+                    nameCell.innerText = curRow.player_name;
 
-    function getLeaderboard() {
-        const xmlhttp = new XMLHttpRequest();
+                    const scoreCell = document.createElement('td');
+                    scoreCell.innerText = curRow.score;
 
-        xmlhttp.onreadystatechange = function() {
-            if (xmlhttp.readyState == XMLHttpRequest.DONE) {
-                if (xmlhttp.status == 200) {
-                    const res = JSON.parse(xmlhttp.responseText);
+                    newRow.appendChild(nameCell);
+                    newRow.appendChild(scoreCell);
 
-                    leaderboardRes = res.leaderboard; 
-                    // update leaderboard table with leaderboardRes content
-                    for (let i = 0; i < leaderboardRes.length; i++) {
-                        const curRow = leaderboardRes[i];
-                        const newRow = document.createElement('tr');
-
-                        const nameCell = document.createElement('td');
-                        nameCell.innerText = curRow.player_name;
-
-                        const scoreCell = document.createElement('td');
-                        scoreCell.innerText = curRow.score;
-
-                        newRow.appendChild(nameCell);
-                        newRow.appendChild(scoreCell);
-
-                        leaderboardHtml.appendChild(newRow);
-                        
-                    }
-                    
+                    leaderboardHtml.appendChild(newRow);
                     
                 }
+                
+                
             }
-        };
-    
-        xmlhttp.open("GET", "./Hangman-api.php?action=getLeaderboard", true);
-        xmlhttp.send();
-    }
+        }
+    };
+
+    xmlhttp.open("GET", "./Hangman-api.php?action=getLeaderboard", true);
+    xmlhttp.send();
+}
